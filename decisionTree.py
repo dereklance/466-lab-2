2aa9e6574ca0e93e0d75696ca7c6ad28190c3e5a
Pat-Farrell-17 <38262281+Pat-Farrell-17@users.noreply.github.com>
2018-10-11 14:27:41 -0700

    Added some xml output for trees returned by the C45 alg

--- decisionTree.py
+++ decisionTree.py
@@ -2,6 +2,7 @@ from TreeNode import Node
 import csvParser, collections, math, sys
 from itertools import groupby
 from operator import itemgetter
+from lxml import etree
 
 def categoryCounts(data):
 	categories = [dict['Category'] for dict in data]
@@ -18,6 +19,38 @@ def entropy(data):
 def isUniform(list):
 	return len(set(list)) <= 1
 
+def printTree(root):
+	print("")
+	print("\t( Node: {})\tLabel: {}".format(root.name, root.label))
+	print("Children:\nLabels:",end="")
+	for child in root.children:
+		print("\t|{} |".format(child.label), end="")
+	print("\n")
+	for child in root.children:
+		print("\t|{} |".format(child.name), end="")
+	print("\n==============================================================")
+	for child in root.children:
+		printTree(child)
+
+
+# Decision nodes not done, haven't checked correctness of nesting on output
+def outputXML(root):
+	#tree = etree.Element("Tree", name="Decision Tree")
+	rootNode = etree.Element("node", var="{}".format(root.name))
+	for child in root.children:
+		edge = etree.Element("edge", var="{}".format(child.label))
+		rootNode.append(edge)
+		childNode = outputXML(child)
+		edge.append(childNode)
+
+	return rootNode
+
+
+
+# Takes a list of dictionaries as data and an attribute
+# and returns a dictionary where each key is a value in
+# the domain of attribute and the values are all data
+# points that have that value in them
 def groupByAttribute(data, attribute):
 	attributeDict = {}
 	for row in data:
@@ -30,12 +63,14 @@ def selectSplittingAttribute(data, attributes, threshold):
 	currentEntropy = entropy(data)
 	bestAttribute = None
 	maxGain = -1
+	#print("\n\n")
 	for attribute in attributes:
 		if attribute == 'Category':
 			continue
 		attributeDict = groupByAttribute(data, attribute)
 		entropyAfterSplit = sum(len(group) / len(data) * entropy(group) for group in attributeDict.values())
 		informationGain = currentEntropy - entropyAfterSplit
+		#print("Information Gain for {}:\t{}".format(attribute, informationGain))
 		if informationGain > maxGain:
 			maxGain = informationGain
 			bestAttribute = attribute
@@ -47,8 +82,8 @@ def build(data, attributes, tree, threshold):
 	if isUniform(dict['Category'] for dict in data):
 		tree.setName(data[0]['Category'])
 	elif len(attributes) == 0:
-		tree.setName(mostFrequentCategory(data))
-	else:
+		tree.setName(mostFrequentCategory(data), None)
+	else:		# Select splitting attribute
 		bestAttribute = selectSplittingAttribute(data, attributes, threshold)
 		if not bestAttribute:
 			tree.setName(mostFrequentCategory(data))
@@ -65,13 +100,14 @@ def build(data, attributes, tree, threshold):
 					build(newData, newAttributes, childNode, threshold)
 
 def main():
-	data = csvParser.parse(sys.argv[1])
+	# CHANGED TO READ FROM SPECIFIC FILE NOT ARGV[1]
+	data = csvParser.parse("trunk/tree03-100-words.csv")
 	attributes = list(data[0].keys())
 	root = Node('Root', None)
 	build(data, attributes, root, 0.01)
-	print(root.name)
-	for child in root.children:
-		print(child.name)
+	s = etree.tostring(outputXML(root), pretty_print=True, encoding='unicode')
+	print(s)
+
 
 
 if __name__ == '__main__':
