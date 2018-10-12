a2e2a21722e3e239493a3df2434c40523d0cb3dc
Pat-Farrell-17 <38262281+Pat-Farrell-17@users.noreply.github.com>
2018-10-12 16:06:52 -0700

    Changed output to follow specs more closely

--- classify.py
+++ classify.py
@@ -1,6 +1,7 @@
 import decisionTree as dt
 import csvParser
 import sys
+import copy
 from TreeNode import Node
 
 # Takes a decision tree and a data point to classify and returns
@@ -27,6 +28,15 @@ def classifyCollection(tree, data):
 
     return classification
 
+def isTrainingSet(data):
+    return ('Category' in data[1].keys())
+
+def removeClassLabels(data):
+    dc = copy.deepcopy(data)
+    for d in dc:
+        d.pop("Category")
+    return dc
+
 def main():
     if not len(sys.argv) == 3:
         print("\t\tMissing arguments\n\tProper Call :\tpython classify.py <CSVFile> <XMLFile>")
@@ -36,16 +46,26 @@ def main():
     dTreeFile = sys.argv[2]
 
     data = csvParser.parse(dataFile)
+    dataB = csvParser.parse("C:/Users/Ian/Documents/CSC466/Lab02/trunk/tree03-100-words.csv")
     attributes = list(data[0].keys())
 
     # Replace this with converting the xml file to tree
     root = Node('Root', None)
-    dt.build(data, attributes, root, 0.01)
+    dt.build(dataB, attributes, root, 0.01)
 
-    classes = classifyCollection(root, data)
-    actualClasses = []
-    for dataPoint in data:
-        actualClasses.append(dataPoint['Category'])
+    if (isTrainingSet(data)):
+        print("\n\tTraining set detected as input, ignoring actual classes " + "during classification")
+
+        actualClasses = []
+        for dataPoint in data:
+            actualClasses.append(dataPoint['Category'])
+        dataC = removeClassLabels(data)
+
+    else:
+        dataC = data
+
+    classes = classifyCollection(root, dataC)
+
 
     print("Print mode : [V]erbose [S]hort\t",end="")
     printMode = input()
@@ -65,6 +85,20 @@ def main():
     else:
         for i, classif in enumerate(classes):
             print("\tClassification for datapoint #{}\t:\t{}".format(i+1, classif[1]))
+
+    if (isTrainingSet(data)):
+        numErr = 0
+        tot = 0
+        for i, classif in enumerate(classes):
+            if not classif[1] == actualClasses[i]:
+                print("Record {} classified as {}, actual class {}".format(i, actualClasses[i], classif[1]))
+                numErr += 1
+            tot = i + 1
+        print("\nTotal records classified:\t\t{}".format(tot))
+        print("# records correctly classified:\t\t{}".format(tot - numErr))
+        print("# records incorrectly classified:\t{}".format(numErr))
+        print("Accuracy:\t{}%".format(((tot - numErr) / tot) * 100))
+        print("Error rate:\t{}%".format(numErr - tot))
 
 
 
